f2c9602904d01f27b707ac635a859948f0b21dd6
el-sayedmahmoud <elsayedmahmoud132001@gmail.com>
2023-11-25 18:38:48 +0200

    Adding About & Work Files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
 import Home from './components/Home';
+import About from "./components/About";
+import Work from "./components/Work";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Home  />
+      <About />
+      <Work />
     </div>
   );
 }
