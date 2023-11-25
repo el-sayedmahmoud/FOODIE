4506be537ed48d0081d9c0c9e05df07d8086a499
el-sayedmahmoud <elsayedmahmoud132001@gmail.com>
2023-11-25 17:28:11 +0200

    Adding Home Section

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,6 +12,21 @@ const Home = () => {
         <div className="home-bannerImage-container">
           <img src={BannerBackground} alt="" />
         </div>
+        <div className="home-text-section">
+          <h1 className="primary-heading">
+            Your Favourite Food Delivered Hot & Fresh
+          </h1>
+          <p className="primary-text">
+            Healthy switcher chefs do all the prep work, like peeding, chopping
+            & marinating, so you can cook a fresh food.
+          </p>
+          <button className="secondary-button">
+            Order Now <FiArrowRight />{" "}
+          </button>
+        </div>
+        <div className="home-image-section">
+          <img src={BannerImage} alt="" />
+        </div>
     </div>   
   </div>
   )
